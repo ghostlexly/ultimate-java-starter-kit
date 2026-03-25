883712cf094a9a448ccae2f819fbb30804962c8e
ghostlexly <ghostlexly@gmail.com>
2026-03-25 14:03:15 +0100

    init

---
+++ src/main/java/com/lunisoft/ultimatejavastarterkit/UltimateJavaStarterKitApplication.java
@@ -0,0 +1,13 @@
+package com.lunisoft.ultimatejavastarterkit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UltimateJavaStarterKitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UltimateJavaStarterKitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lunisoft/ultimatejavastarterkit/UltimateJavaStarterKitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lunisoft.ultimatejavastarterkit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UltimateJavaStarterKitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
