1961763ce221c5a8cfc5f4a00c1c2518d874838a
ghostlexly <ghostlexly@gmail.com>
2026-04-22 16:43:48 +0200

    chore(backend): format `Account` entity for code readability, add `@Job` annotation to `DemoJobRunrEnqueueJob`

--- backend/src/main/java/com/lunisoft/javastarter/module/account/entity/Account.java
+++ backend/src/main/java/com/lunisoft/javastarter/module/account/entity/Account.java
@@ -6,46 +6,45 @@ import com.lunisoft.javastarter.module.auth.entity.VerificationToken;
 import com.lunisoft.javastarter.module.customer.entity.Customer;
 import com.lunisoft.javastarter.shared.entity.BaseEntity;
 import jakarta.persistence.*;
+import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
-
-import java.util.List;
 
 @Entity
 @Getter
 @Setter
 @Table(
-        name = "account",
-        uniqueConstraints = {
-                @UniqueConstraint(columnNames = {"provider_id", "provider_account_id", "role"})
-        })
+    name = "account",
+    uniqueConstraints = {
+      @UniqueConstraint(columnNames = {"provider_id", "provider_account_id", "role"})
+    })
 public class Account extends BaseEntity {
 
-    @Column(nullable = false, unique = true)
-    private String email;
+  @Column(nullable = false, unique = true)
+  private String email;
 
-    private String password;
+  private String password;
 
-    @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
-    private Role role;
+  @Enumerated(EnumType.STRING)
+  @Column(nullable = false)
+  private Role role;
 
-    private String providerId;
+  private String providerId;
 
-    private String providerAccountId;
+  private String providerAccountId;
 
-    @Column(nullable = false)
-    private boolean emailVerified = false;
+  @Column(nullable = false)
+  private boolean emailVerified = false;
 
-    @OneToMany(mappedBy = "account", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Session> sessions;
+  @OneToMany(mappedBy = "account", cascade = CascadeType.ALL, orphanRemoval = true)
+  private List<Session> sessions;
 
-    @OneToOne(mappedBy = "account", cascade = CascadeType.ALL, orphanRemoval = true)
-    private Customer customer;
+  @OneToOne(mappedBy = "account", cascade = CascadeType.ALL, orphanRemoval = true)
+  private Customer customer;
 
-    @OneToOne(mappedBy = "account", cascade = CascadeType.ALL, orphanRemoval = true)
-    private Admin admin;
+  @OneToOne(mappedBy = "account", cascade = CascadeType.ALL, orphanRemoval = true)
+  private Admin admin;
 
-    @OneToMany(mappedBy = "account", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<VerificationToken> verificationTokens;
+  @OneToMany(mappedBy = "account", cascade = CascadeType.ALL, orphanRemoval = true)
+  private List<VerificationToken> verificationTokens;
 }
--- backend/src/main/java/com/lunisoft/javastarter/module/demo/usecase/enqueuejob/DemoJobRunrEnqueueJob.java
+++ backend/src/main/java/com/lunisoft/javastarter/module/demo/usecase/enqueuejob/DemoJobRunrEnqueueJob.java
@@ -1,6 +1,8 @@
 package com.lunisoft.javastarter.module.demo.usecase.enqueuejob;
 
 import java.time.Duration;
+
+import org.jobrunr.jobs.annotations.Job;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -10,6 +12,7 @@ public class DemoJobRunrEnqueueJob {
 
   private static final Logger log = LoggerFactory.getLogger(DemoJobRunrEnqueueJob.class);
 
+  @Job(name = "demo-job", retries = 3)
   public void execute(String example) throws InterruptedException {
     log.info("Job started with example: {} !", example);
 
