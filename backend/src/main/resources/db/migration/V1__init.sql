ab1fa76c1dd77c9f9ff3d2316e797ccdf8f43247
ghostlexly <ghostlexly@gmail.com>
2026-05-04 18:57:29 +0200

    chore(backend): rename database tables and constraints to use pluralized naming convention

--- backend/src/main/resources/db/migration/V1__init.sql
+++ backend/src/main/resources/db/migration/V1__init.sql
@@ -1,4 +1,4 @@
-CREATE TABLE account
+CREATE TABLE accounts
 (
     id                  UUID         NOT NULL,
     created_at          TIMESTAMP WITHOUT TIME ZONE NOT NULL,
@@ -8,36 +8,36 @@ CREATE TABLE account
     role                VARCHAR(255) NOT NULL,
     provider_id         VARCHAR(255),
     provider_account_id VARCHAR(255),
-    email_verified   BOOLEAN      NOT NULL,
-    CONSTRAINT pk_account PRIMARY KEY (id)
+    email_verified      BOOLEAN      NOT NULL,
+    CONSTRAINT pk_accounts PRIMARY KEY (id)
 );
 
-CREATE TABLE admin
+CREATE TABLE admins
 (
     id         UUID NOT NULL,
     created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL,
     updated_at TIMESTAMP WITHOUT TIME ZONE NOT NULL,
     account_id UUID NOT NULL,
-    CONSTRAINT pk_admin PRIMARY KEY (id)
+    CONSTRAINT pk_admins PRIMARY KEY (id)
 );
 
-CREATE TABLE app_config
+CREATE TABLE app_configs
 (
     id         UUID         NOT NULL,
     created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL,
     updated_at TIMESTAMP WITHOUT TIME ZONE NOT NULL,
     key        VARCHAR(255) NOT NULL,
     value      VARCHAR(255),
-    CONSTRAINT pk_app_config PRIMARY KEY (id)
+    CONSTRAINT pk_app_configs PRIMARY KEY (id)
 );
 
-CREATE TABLE customer
+CREATE TABLE customers
 (
-    id           UUID NOT NULL,
-    created_at   TIMESTAMP WITHOUT TIME ZONE NOT NULL,
-    updated_at   TIMESTAMP WITHOUT TIME ZONE NOT NULL,
-    account_id   UUID NOT NULL,
-    CONSTRAINT pk_customer PRIMARY KEY (id)
+    id         UUID NOT NULL,
+    created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+    updated_at TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+    account_id UUID NOT NULL,
+    CONSTRAINT pk_customers PRIMARY KEY (id)
 );
 
 CREATE TABLE media
@@ -52,7 +52,7 @@ CREATE TABLE media
     CONSTRAINT pk_media PRIMARY KEY (id)
 );
 
-CREATE TABLE session
+CREATE TABLE sessions
 (
     id         UUID NOT NULL,
     created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL,
@@ -61,10 +61,10 @@ CREATE TABLE session
     ip_address VARCHAR(255),
     user_agent VARCHAR(255),
     expires_at TIMESTAMP WITHOUT TIME ZONE NOT NULL,
-    CONSTRAINT pk_session PRIMARY KEY (id)
+    CONSTRAINT pk_sessions PRIMARY KEY (id)
 );
 
-CREATE TABLE verification_token
+CREATE TABLE verification_tokens
 (
     id         UUID         NOT NULL,
     created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL,
@@ -75,35 +75,35 @@ CREATE TABLE verification_token
     attempts   INTEGER      NOT NULL,
     account_id UUID         NOT NULL,
     expires_at TIMESTAMP WITHOUT TIME ZONE NOT NULL,
-    CONSTRAINT pk_verification_token PRIMARY KEY (id)
+    CONSTRAINT pk_verification_tokens PRIMARY KEY (id)
 );
 
-ALTER TABLE account
-    ADD CONSTRAINT uc_8c4cf4ce83e03f7d46b0ee47c UNIQUE (provider_id, provider_account_id, role);
+ALTER TABLE accounts
+    ADD CONSTRAINT uc_6db797e7dab5534cb41591a18 UNIQUE (provider_id, provider_account_id, role);
 
-ALTER TABLE account
-    ADD CONSTRAINT uc_account_email UNIQUE (email);
+ALTER TABLE accounts
+    ADD CONSTRAINT uc_accounts_email UNIQUE (email);
 
-ALTER TABLE admin
-    ADD CONSTRAINT uc_admin_account UNIQUE (account_id);
+ALTER TABLE admins
+    ADD CONSTRAINT uc_admins_account UNIQUE (account_id);
 
-ALTER TABLE app_config
-    ADD CONSTRAINT uc_app_config_key UNIQUE (key);
+ALTER TABLE app_configs
+    ADD CONSTRAINT uc_app_configs_key UNIQUE (key);
 
-ALTER TABLE customer
-    ADD CONSTRAINT uc_customer_account UNIQUE (account_id);
+ALTER TABLE customers
+    ADD CONSTRAINT uc_customers_account UNIQUE (account_id);
 
-ALTER TABLE verification_token
-    ADD CONSTRAINT uc_verification_token_token UNIQUE (token);
+ALTER TABLE verification_tokens
+    ADD CONSTRAINT uc_verification_tokens_token UNIQUE (token);
 
-ALTER TABLE admin
-    ADD CONSTRAINT FK_ADMIN_ON_ACCOUNT FOREIGN KEY (account_id) REFERENCES account (id);
+ALTER TABLE admins
+    ADD CONSTRAINT FK_ADMINS_ON_ACCOUNT FOREIGN KEY (account_id) REFERENCES accounts (id);
 
-ALTER TABLE customer
-    ADD CONSTRAINT FK_CUSTOMER_ON_ACCOUNT FOREIGN KEY (account_id) REFERENCES account (id);
+ALTER TABLE customers
+    ADD CONSTRAINT FK_CUSTOMERS_ON_ACCOUNT FOREIGN KEY (account_id) REFERENCES accounts (id);
 
-ALTER TABLE session
-    ADD CONSTRAINT FK_SESSION_ON_ACCOUNT FOREIGN KEY (account_id) REFERENCES account (id);
+ALTER TABLE sessions
+    ADD CONSTRAINT FK_SESSIONS_ON_ACCOUNT FOREIGN KEY (account_id) REFERENCES accounts (id);
 
-ALTER TABLE verification_token
-    ADD CONSTRAINT FK_VERIFICATION_TOKEN_ON_ACCOUNT FOREIGN KEY (account_id) REFERENCES account (id);
+ALTER TABLE verification_tokens
+    ADD CONSTRAINT FK_VERIFICATION_TOKENS_ON_ACCOUNT FOREIGN KEY (account_id) REFERENCES accounts (id);
